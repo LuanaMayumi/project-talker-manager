56b7965ecfca51130d2c4ee8c1376616e3c56687
LuanaMayumi <luh.mayumi@gmail.com>
2023-04-14 17:37:32 -0300

    requisito 3

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -3,7 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  res.status(200).json({ message: 'teste' });
+  const { email, password } = req.body;
+  const num = Math.random();
+  const string = num.toString();
+  const token = string.slice(-16);
+  console.log(token);
+  if (email && password) {
+    res.status(200).json({ token: `${token}` });
+  }
 });
 
 module.exports = router;
